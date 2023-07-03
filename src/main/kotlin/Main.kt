90cbebc45c3bcca1dbc08e3cfaa904b35cc759de
Ryan Marvin <ryanflex2003@gmail.com>
2023-07-03 17:28:15 -0500

    handling of retrieval of available album ids

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,28 @@
 import services.PhotoService
+
 fun main() {
     val photoService = PhotoService()
     var albumId = 0
 
+    print("Enter 0 to view available albums or enter an album id to view its photos:")
     albumId = readInput()
 
-    val response = photoService.getPhotos(albumId)
+    if (albumId == 0) {
+        println("Available album ids:${photoService.getAlbumIds()}")
+        albumId = readInput(true)
+    }
 
+    val response = photoService.getPhotos(albumId)
     println(response)
 }
 
-fun readInput() : Int {
+fun readInput(askToEnter: Boolean = false) : Int {
     return try {
-        print("Enter 0 to view available albums or enter an album id to view its photos:")
+        if (askToEnter)
+        print("Enter an album id to view its photos:")
         readln().toInt()
     } catch (ex: Exception) {
-        println("Enter a valid album Id")
+        print("Enter a valid album id:")
         readInput()
     }
 }
