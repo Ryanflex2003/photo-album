2d3cf62641b80e6913b0bad1e841afd86665a6dc
Ryan Marvin <ryanflex2003@gmail.com>
2023-07-04 02:50:19 -0500

    adding dependency injection, tests for main, and wrapper for reading input stream for mocking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,42 @@
 import services.PhotoService
 
 fun main() {
-    val photoService = PhotoService()
-    var albumId = 0
-
-    print("Enter 0 to view available albums or enter an album id to view its photos:")
-    albumId = readInput()
-
-    if (albumId == 0) {
-        println("Available album ids:${photoService.getAlbumIds()}")
-        albumId = readInput(true)
-    }
-
-    val response = photoService.getPhotos(albumId)
-    println(response)
+    val program = Program()
+    program.execute()
 }
 
-fun readInput(askToEnter: Boolean = false) : Int {
-    return try {
-        if (askToEnter)
-        print("Enter an album id to view its photos:")
-        readln().toInt()
-    } catch (ex: Exception) {
-        print("Enter a valid album id:")
-        readInput()
+class Program(
+    private var readWrapper: ReadWrapper = ReadWrapper(),
+    private var photoService: PhotoService = PhotoService()
+) {
+    fun execute() {
+        var albumId : Int
+        print("Enter 0 to view available albums or enter an album id to view its photos:")
+        albumId = readInput()
+
+        if (albumId == 0) {
+            println("Available album ids:${photoService.getAlbumIds()}")
+            albumId = readInput(true)
+        }
+
+        val response = photoService.getPhotos(albumId)
+        println(response)
+    }
+
+    fun readInput(askToEnter: Boolean = false) : Int {
+        return try {
+            if (askToEnter)
+                print("Enter an album id to view its photos:")
+            readWrapper.readLine().toInt()
+        } catch (ex: Exception) {
+            print("Enter a valid album id:")
+            readInput()
+        }
+    }
+}
+
+class ReadWrapper() {
+    fun readLine() : String {
+         return readln()
     }
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,9 +1,31 @@
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
+import services.PhotoService
 
 class MainTest {
+    @Test
+    fun `Verify given no album ids when album id of 0 entered album ids are listed`() {
+        val mockReadWrapper = mockk<ReadWrapper>()
+        val mockPhotoService = mockk<PhotoService>()
+        every { mockReadWrapper.readLine() } returns "0"
+        every { mockPhotoService.getAlbumIds() } returns ""
+        every { mockPhotoService.getPhotos(any()) } returns ""
+        val program = Program(mockReadWrapper, mockPhotoService)
+        program.execute()
+        verify (exactly = 1) { mockPhotoService.getAlbumIds()}
+    }
 
     @Test
-    fun `Verify all methods are executed`() {
-
+    fun `Verify given an album id when album id NOT 0 entered album ids are not returned`() {
+        val mockReadWrapper = mockk<ReadWrapper>()
+        val mockPhotoService = mockk<PhotoService>()
+        every { mockReadWrapper.readLine() } returns "1"
+        every { mockPhotoService.getPhotos(1) } returns ""
+        val program = Program(mockReadWrapper, mockPhotoService)
+        program.execute()
+        verify (exactly = 0) { mockPhotoService.getAlbumIds()}
+        verify (exactly = 1) { mockPhotoService.getPhotos(1) }
     }
 }
